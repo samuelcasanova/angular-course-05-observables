7708d0b1184d1254a482ca6cb4c19923d0710af0
samuel.casanova <samuel.casanova@immfly.com>
2023-12-24 19:12:55 +0100

    add custom observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription, interval } from 'rxjs';
+import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +8,32 @@ import { Subscription, interval } from 'rxjs';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  subscription: Subscription;
+  subscription: Subscription
+  customSubscription: Subscription
 
   constructor() { }
 
   ngOnInit() {
-    this.subscription = interval(1000).subscribe(count => { console.log(count)})
+    this.subscription = interval(1000).subscribe(count => { console.log(`interval observable count=${count}`)})
+    const customObservable = new Observable(observer => {
+      let count = 0
+      setInterval(() => {
+        if (count > 5) {
+          observer.error(new Error('Count is greater than 5, its horrible'))
+        } else {
+          observer.next(count++)
+        }
+      }, 1000)
+    })
+    this.customSubscription = customObservable.subscribe(data => {
+      console.log(`custom observable data=${data}`)
+    }, error => {
+      console.error(error)
+    })
   }
 
   ngOnDestroy() {
     this.subscription?.unsubscribe()
+    this.customSubscription?.unsubscribe()
   }
 }
