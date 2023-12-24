9ec687ec7afb333fdd53528bb9865ebaaa42bcc1
samuel.casanova <samuel.casanova@immfly.com>
2023-12-24 19:47:47 +0100

    add subject as an alternative of event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  activeSubject: Subject<boolean>
 
-  ngOnInit() {}
+  constructor() {}
+  
+  ngOnInit() {
+    this.activeSubject = new Subject<boolean>()
+    this.activeSubject.subscribe(isActive => {
+      console.log(`Activated=${isActive}`)
+    })
+  }
+
+  onClickActivate() {
+    this.activeSubject.next(true)
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription, interval } from 'rxjs';
 import { Observable } from 'rxjs-compat';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -18,18 +19,26 @@ export class HomeComponent implements OnInit, OnDestroy {
     const customObservable = new Observable(observer => {
       let count = 0
       setInterval(() => {
-        if (count > 5) {
+        if (count === 30) {
+          observer.complete()
+        }
+        if (count > 50) {
           observer.error(new Error('Count is greater than 5, its horrible'))
         } else {
           observer.next(count++)
         }
       }, 1000)
     })
-    this.customSubscription = customObservable.subscribe(data => {
-      console.log(`custom observable data=${data}`)
-    }, error => {
-      console.error(error)
-    })
+    this.customSubscription = customObservable.pipe(
+      filter((data: number) => data%2 === 0),
+      map((data: number) => `(${data})`))
+      .subscribe(data => {
+        console.log(`custom observable data=${data}`)
+      }, error => {
+        console.error(error)
+      }, () => {
+        console.log('COMPLETED!')
+      })
   }
 
   ngOnDestroy() {
